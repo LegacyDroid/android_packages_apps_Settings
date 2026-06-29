e3d4a33178e4e81e3b0f138fde5bdf8039803c17
minhmc2007 <quangminh21072010@gmail.com>
2026-06-29 22:15:16 +0700

    Move spring physics toggle from LegacyDex to Appearance
    Change-Id: I593e456af253417a7dbba89616964685f67b0ab3

--- src/com/android/settings/legacydroid/AppearanceSettings.java
+++ src/com/android/settings/legacydroid/AppearanceSettings.java
@@ -3,6 +3,9 @@ package com.android.settings.legacydroid;
 import android.app.settings.SettingsEnums;
 import android.content.Context;
 import android.os.Bundle;
+import android.os.SystemProperties;
+
+import androidx.preference.SwitchPreference;
 
 import com.android.settings.R;
 import com.android.settings.dashboard.DashboardFragment;
@@ -14,6 +17,8 @@ import java.util.List;
 public class AppearanceSettings extends DashboardFragment {
 
     private static final String TAG = "AppearanceSettings";
+    private static final String PROP_FLUID_ANIM = "persist.sys.fluid_animations.enabled";
+    private static final String KEY_FLUID_ANIM = "fluid_animations";
 
     @Override
     public int getMetricsCategory() {
@@ -43,5 +48,14 @@ public class AppearanceSettings extends DashboardFragment {
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         super.onCreatePreferences(savedInstanceState, rootKey);
+
+        SwitchPreference fluidAnim = findPreference(KEY_FLUID_ANIM);
+        if (fluidAnim != null) {
+            fluidAnim.setChecked(SystemProperties.getBoolean(PROP_FLUID_ANIM, false));
+            fluidAnim.setOnPreferenceChangeListener((pref, newValue) -> {
+                SystemProperties.set(PROP_FLUID_ANIM, (Boolean) newValue ? "true" : "false");
+                return true;
+            });
+        }
     }
 }
