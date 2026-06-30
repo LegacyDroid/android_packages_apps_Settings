567abc05cd360dcda25c4b3e0d32a6c9ff3eb5b7
minhmc2007 <quangminh21072010@gmail.com>
2026-06-30 09:49:46 +0700

    Settings: remove fluid animation references
    Change-Id: I184931627dea326e0a25eadb7b14f549ee6b1fa3

--- src/com/android/settings/legacydroid/AppearanceSettings.java
+++ src/com/android/settings/legacydroid/AppearanceSettings.java
@@ -2,10 +2,6 @@ package com.android.settings.legacydroid;
 
 import android.app.settings.SettingsEnums;
 import android.content.Context;
-import android.os.Bundle;
-import android.os.SystemProperties;
-
-import androidx.preference.SwitchPreference;
 
 import com.android.settings.R;
 import com.android.settings.dashboard.DashboardFragment;
@@ -17,8 +13,6 @@ import java.util.List;
 public class AppearanceSettings extends DashboardFragment {
 
     private static final String TAG = "AppearanceSettings";
-    private static final String PROP_FLUID_ANIM = "persist.sys.fluid_animations.enabled";
-    private static final String KEY_FLUID_ANIM = "fluid_animations";
 
     @Override
     public int getMetricsCategory() {
@@ -43,19 +37,5 @@ public class AppearanceSettings extends DashboardFragment {
     @Override
     protected List<AbstractPreferenceController> createPreferenceControllers(Context context) {
         return new ArrayList<>();
-    }
-
-    @Override
-    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-        super.onCreatePreferences(savedInstanceState, rootKey);
-
-        SwitchPreference fluidAnim = findPreference(KEY_FLUID_ANIM);
-        if (fluidAnim != null) {
-            fluidAnim.setChecked(SystemProperties.getBoolean(PROP_FLUID_ANIM, false));
-            fluidAnim.setOnPreferenceChangeListener((pref, newValue) -> {
-                SystemProperties.set(PROP_FLUID_ANIM, (Boolean) newValue ? "true" : "false");
-                return true;
-            });
-        }
     }
 }
