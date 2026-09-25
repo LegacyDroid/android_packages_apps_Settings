45aa9823c62d9b0b655b15123589e392f0ff8c6a
minhmc2007 <quangminh21072010@gmail.com>
2026-09-25 21:00:12 +0700

    settings: add Liquid Glass toggle
    Change-Id: Ib2df90bb466e42db9df8f7ed17d23b732946de7f

--- src/com/android/settings/legacydroid/LegacydroidAppearanceFragment.java
+++ src/com/android/settings/legacydroid/LegacydroidAppearanceFragment.java
@@ -6,6 +6,10 @@
 package com.android.settings.legacydroid;
 
 import android.app.settings.SettingsEnums;
+import android.os.Bundle;
+import android.provider.Settings;
+
+import androidx.preference.SwitchPreferenceCompat;
 
 import com.android.settings.R;
 import com.android.settings.dashboard.DashboardFragment;
@@ -14,6 +18,9 @@ import com.android.settings.dashboard.DashboardFragment;
 public class LegacydroidAppearanceFragment extends DashboardFragment {
 
     private static final String TAG = "LegacydroidAppearance";
+
+    private static final String KEY_LIQUID_GLASS = "legacydroid_liquid_glass";
+    private static final String SETTING_LIQUID_GLASS = KEY_LIQUID_GLASS;
 
     @Override
     protected int getPreferenceScreenResId() {
@@ -28,5 +35,25 @@ public class LegacydroidAppearanceFragment extends DashboardFragment {
     @Override
     public int getMetricsCategory() {
         return SettingsEnums.SETTINGS_HOMEPAGE;
+    }
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        super.onCreatePreferences(savedInstanceState, rootKey);
+        initLiquidGlassPreference();
+    }
+
+    private void initLiquidGlassPreference() {
+        final SwitchPreferenceCompat liquidGlass = findPreference(KEY_LIQUID_GLASS);
+        if (liquidGlass == null) {
+            return;
+        }
+        liquidGlass.setChecked(Settings.Global.getInt(
+                getContentResolver(), SETTING_LIQUID_GLASS, 1) == 1);
+        liquidGlass.setOnPreferenceChangeListener((preference, newValue) -> {
+            Settings.Global.putInt(getContentResolver(), SETTING_LIQUID_GLASS,
+                    (Boolean) newValue ? 1 : 0);
+            return true;
+        });
     }
 }
